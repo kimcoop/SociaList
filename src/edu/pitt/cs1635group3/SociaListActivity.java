5807bf997c4fc55c21e63323fe05f1791aa47d6c
James Cervone <Jim@dhcp-lp-130-49-27-48.oakland.resnet.pitt.edu>
2012-03-18 03:00:48 -0400

    UI skeleton created. Added file list.xml

--- src/edu/pitt/cs1635group3/SociaListActivity.java
+++ src/edu/pitt/cs1635group3/SociaListActivity.java
@@ -7,6 +7,9 @@ public class SociaListActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	
+    	
+    	
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
