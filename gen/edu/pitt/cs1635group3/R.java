bb02c2531b651a0b16c4bec79a648b0fe3d1ef48
James Cervone <Jim@fq-wireless-pittnet-150-212-2-16.wireless.pitt.edu>
2012-03-18 15:35:35 -0400

    changed size of list text

--- gen/edu/pitt/cs1635group3/R.java
+++ gen/edu/pitt/cs1635group3/R.java
@@ -13,11 +13,40 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int assign_button=0x7f050002;
+        public static final int checkBox1=0x7f050007;
+        public static final int checkBox2=0x7f05000a;
+        public static final int checkBox3=0x7f05000d;
+        public static final int checkBox4=0x7f050010;
+        public static final int complete_button=0x7f050003;
+        public static final int invite_button=0x7f050004;
+        public static final int linearLayout1=0x7f050001;
+        public static final int tableLayout1=0x7f050005;
+        public static final int tableRow1=0x7f050006;
+        public static final int tableRow2=0x7f050009;
+        public static final int tableRow3=0x7f05000c;
+        public static final int tableRow4=0x7f05000f;
+        public static final int textView1=0x7f050000;
+        public static final int textView2=0x7f050008;
+        public static final int textView3=0x7f05000b;
+        public static final int textView4=0x7f05000e;
+        public static final int textView5=0x7f050011;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int list=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int assign_button=0x7f040004;
+        public static final int complete_button=0x7f040005;
         public static final int hello=0x7f040000;
+        /**  Buttons 
+         */
+        public static final int invite_button=0x7f040003;
+        /**  Headers 
+         */
+        public static final int lists=0x7f040002;
     }
 }
