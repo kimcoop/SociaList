f04bc681ef4dcc8852933108315a826822d5104e
Kim Cooperrider <kim.cooperrider@gmail.com>
2012-03-19 21:18:06 -0400

    Dynamically loading data from server & list adapter beginnings

--- gen/edu/pitt/cs1635group3/R.java
+++ gen/edu/pitt/cs1635group3/R.java
@@ -21,6 +21,8 @@ public final class R {
         public static final int checkBox4=0x7f050010;
         public static final int complete_button=0x7f050003;
         public static final int invite_button=0x7f050004;
+        public static final int item_subtitle=0x7f050013;
+        public static final int item_title=0x7f050012;
         public static final int linearLayout1=0x7f050001;
         public static final int tableLayout1=0x7f050005;
         public static final int tableRow1=0x7f050006;
@@ -35,7 +37,9 @@ public final class R {
     }
     public static final class layout {
         public static final int list=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int listplaceholder=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int orig_main=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- src/edu/pitt/cs1635group3/SociaListActivity.java
+++ src/edu/pitt/cs1635group3/SociaListActivity.java
@@ -1,16 +1,74 @@
 package edu.pitt.cs1635group3;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public class SociaListActivity extends Activity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+public class SociaListActivity extends ListActivity {
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-    	
-    	
+    public void onCreate(Bundle savedInstanceState) {    	
     	
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.listplaceholder);
+
+    	ArrayList<HashMap<String, String>> mylist = new ArrayList<HashMap<String, String>>();
+
+    	//Get the data (see above)
+    	JSONObject json =
+    		JSONfunctions.getJSONfromURL("http://www.zebrafishtec.com/index.json");
+
+    	       try{
+    		JSONArray  lists = json.getJSONArray("lists");
+
+    	      	       	//Loop the Array
+    	        for(int i=0;i < lists.length()-1; i++){						
+
+    	        	HashMap<String, String> map = new HashMap<String, String>();
+    	        	JSONObject e = lists.getJSONObject(i);
+
+    	        	map.put("id",  String.valueOf(i));
+    	        	map.put("name", e.getString("name"));
+    	        	map.put("updated", "Last updated: " +  e.getString("datetime"));
+    	        	mylist.add(map);
+    		}
+    	       }catch(JSONException e)        {
+    	       	 Log.e("log_tag", "Error parsing data "+e.toString());
+    	       }
+    	       
+        ListAdapter adapter = new SimpleAdapter(this, mylist , R.layout.main,
+                new String[] { "name", "updated" },
+                new int[] { R.id.item_title, R.id.item_subtitle });
+
+		setListAdapter(adapter);
+		
+		final ListView lv = getListView();
+		lv.setTextFilterEnabled(true);
+		lv.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				@SuppressWarnings("unchecked")
+				HashMap<String, String> o = (HashMap<String, String>) lv.getItemAtPosition(position);
+				Toast.makeText(getBaseContext(), "ID '" + o.get("id") + "' was clicked.", Toast.LENGTH_SHORT).show(); 
+		
+			}
+		});
+        
+        
+        
     }
+    
 }
