edb4711d724e7971fa87f3bc7deb5611bfe500b6
Kim Cooperrider <kim.cooperrider@gmail.com>
2012-03-21 23:47:41 -0400

    added buttons within createListActivity for adding,saving,deleting list

--- src/edu/pitt/cs1635group3/CreateListActivity.java
+++ src/edu/pitt/cs1635group3/CreateListActivity.java
@@ -4,6 +4,7 @@ package edu.pitt.cs1635group3;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.ListView;
@@ -19,9 +20,23 @@ public class CreateListActivity extends Activity {
 	        setContentView(R.layout.editlist);
 	 
 	        EditText listName = (EditText) findViewById(R.id.list_name);
-			Toast.makeText(getBaseContext(), "Inside CreateListActivity", Toast.LENGTH_SHORT).show(); 
+	        listName.setText("List name here");
 
 		
 	}
+	    
+	    public void addListItem(View v) {
+			Toast.makeText(getBaseContext(), "TODO: add list item", Toast.LENGTH_SHORT).show(); 
 
+	    }
+
+	    public void saveList(View v) {
+			Toast.makeText(getBaseContext(), "TODO: save list", Toast.LENGTH_SHORT).show(); 
+
+	    }
+	    
+	    public void deleteList(View v) {
+			Toast.makeText(getBaseContext(), "TODO: delete list", Toast.LENGTH_SHORT).show(); 
+
+	    }
 }
