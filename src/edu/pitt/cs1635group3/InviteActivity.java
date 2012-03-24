6da0913f586cc7be15b133c5bb7567ee6e5d2e82
Yuxin Liu <brendanliu8899@yahoo.com>
2012-03-25 07:20:12 +0800

    more work on invite feature

--- src/edu/pitt/cs1635group3/InviteActivity.java
+++ src/edu/pitt/cs1635group3/InviteActivity.java
@@ -17,20 +17,26 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract.Contacts;
 import android.provider.ContactsContract.CommonDataKinds.Email;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class InviteActivity extends Activity {
 
-	 private static final String DEBUG_TAG = "InviteActivity";
+	 	private static final String DEBUG_TAG = "InviteActivity";
 	    private static final int CONTACT_PICKER_RESULT = 1001;
+	    
+	    String massage = "";
+	    String email = "";
+        String phone = "";
 
 	    @Override
 	    protected void onCreate(Bundle savedInstanceState) {
@@ -47,10 +53,14 @@ public class InviteActivity extends Activity {
 	    @Override
 	    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 	        if (resultCode == RESULT_OK) {
-	            switch (requestCode) {
-	            case CONTACT_PICKER_RESULT:
 	                Cursor cursor = null;
-	                String email = "";
+	                Cursor cursorPhone = null;
+	                
+	                Spinner spinner = (Spinner) findViewById(R.id.invite_type_spinner);
+	                String invite_type = (String) spinner.getItemAtPosition(spinner.getSelectedItemPosition());
+	                
+	                
+	                
 	                try {
 	                    Bundle extras = data.getExtras();
 	                    Set<String> keys = extras.keySet();
@@ -66,53 +76,115 @@ public class InviteActivity extends Activity {
 
 	                    // get the contact id from the Uri
 	                    String id = result.getLastPathSegment();
+	                    
+	                    if(invite_type.equals("Send Email")){
+		                    // query for everything email
+		                    cursor = getContentResolver().query(Email.CONTENT_URI,
+		                            null, Email.CONTACT_ID + "=?", new String[] { id },
+		                            null);
+		                    int emailIdx = cursor.getColumnIndex(Email.DATA);
+		                    
+		                    // let's just get the first email
+		                    if (cursor.moveToFirst()) {
 
-	                    // query for everything email
-	                    cursor = getContentResolver().query(Email.CONTENT_URI,
-	                            null, Email.CONTACT_ID + "=?", new String[] { id },
-	                            null);
+		                        
+		                          //Iterate all columns. :) 
+		                          String columns[] = cursor.getColumnNames(); 
+		                          for (String column : columns) { 
+		                        	  int index = cursor.getColumnIndex(column);
+		                        	  Log.v(DEBUG_TAG, "Column: " + column + " == [" +
+		                        			  cursor.getString(index) + "]"); 
+		                          }
+		                         
 
-	                    int emailIdx = cursor.getColumnIndex(Email.DATA);
+		                        email = cursor.getString(emailIdx);
+		                        
+		                        Log.v(DEBUG_TAG, "Got email: " + email);
+		                        
 
-	                    // let's just get the first email
-	                    if (cursor.moveToFirst()) {
-
-	                        /*
-	                         * Iterate all columns. :) String columns[] =
-	                         * cursor.getColumnNames(); for (String column :
-	                         * columns) { int index = cursor.getColumnIndex(column);
-	                         * Log.v(DEBUG_TAG, "Column: " + column + " == [" +
-	                         * cursor.getString(index) + "]"); }
-	                         */
-
-	                        email = cursor.getString(emailIdx);
-
-	                        Log.v(DEBUG_TAG, "Got email: " + email);
-
-	                    } else {
-	                        Log.w(DEBUG_TAG, "No results");
+		                    } else {
+		                        Log.w(DEBUG_TAG, "No email results");
+		                    }
 	                    }
+	                    
+	                    if(invite_type.equals("Send SMS")){
+		                    // query for everything phone
+		                    cursorPhone = getContentResolver().query(Phone.CONTENT_URI,
+		                            null, Phone.CONTACT_ID + "=?", new String[] { id },
+		                            null);
+		                    
+		                    int phoneIdx = cursorPhone.getColumnIndex(Phone.DATA);
+		                    // let's just get the first phone
+		                    if (cursorPhone.moveToFirst()) {
+		                          //Iterate all columns. :) 
+		                          String columnsPhone[] = cursorPhone.getColumnNames(); 
+		                          for (String column : columnsPhone) { 
+		                        	  int index = cursorPhone.getColumnIndex(column);
+		                        	  Log.v(DEBUG_TAG, "Column: " + column + " == [" +
+		                        			  cursorPhone.getString(index) + "]"); 
+		                          }
+		                        
+		                        phone = cursorPhone.getString(phoneIdx);
+		                        Log.v(DEBUG_TAG, "Got phone: " + phone);
+
+		                    } else {
+		                        Log.w(DEBUG_TAG, "No phone results");
+		                    }
+	                    }
+
+	                   
+	                    
 	                } catch (Exception e) {
-	                    Log.e(DEBUG_TAG, "Failed to get email data", e);
+	                    Log.e(DEBUG_TAG, "Failed to get data", e);
 	                } finally {
 	                    if (cursor != null) {
 	                        cursor.close();
 	                    }
-	                    EditText emailEntry = (EditText) findViewById(R.id.invite_email);
-	                    emailEntry.setText(email);
-	                    if (email.length() == 0) {
-	                        Toast.makeText(this, "No email found for contact.",
-	                                Toast.LENGTH_LONG).show();
+	                    if(invite_type.equals("Send Email")){
+		                    EditText emailEntry = (EditText) findViewById(R.id.invite_email);
+		                    emailEntry.setText(email);
+		                    if (email.length() == 0) {
+		                        Toast.makeText(this, "No email found for contact.",
+		                                Toast.LENGTH_LONG).show();
+		                    }
+	                    }
+	                    
+	                    if(invite_type.equals("Send SMS")){
+	                    	EditText massageEntry = (EditText) findViewById(R.id.invite_email);
+	                    	massageEntry.setText(phone);
+	                    	
+	                    	if (phone.length() == 0) {
+		                        Toast.makeText(this, "No phone found for contact.",
+		                                Toast.LENGTH_LONG).show();
+		                    }
 	                    }
 
 	                }
 
-	                break;
-	            }
-
 	        } else {
 	            Log.w(DEBUG_TAG, "Warning: activity result not ok");
 	        }
+	    }
+	    
+	    public void sendInvitation(View v){
+	    	Spinner spinner = (Spinner) findViewById(R.id.invite_type_spinner);
+            String invite_type = (String) spinner.getItemAtPosition(spinner.getSelectedItemPosition());
+            
+            if(invite_type.equals("Send SMS")){
+            	Uri smsUri = Uri.parse("tel:"+phone);
+            	Intent sendIntent = new Intent(Intent.ACTION_VIEW,smsUri);
+            	sendIntent.putExtra("sms_body", "The SMS text SociaList!!!"); 
+            	sendIntent.setType("vnd.android-dir/mms-sms");
+            	startActivity(sendIntent);   
+            	
+            
+            } else if(invite_type.equals("Send Email")){
+            	
+            }
+            
+            
+	    	
+	    	
 	    }
 
 }
