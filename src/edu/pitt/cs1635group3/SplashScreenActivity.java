bdf6237ff3454eef909d03ce048aa67c45047a0e
Rob Filippi <flippi273@gmail.com>
2012-03-25 20:03:03 -0400

    Crappy splash screen added! YAY

---
+++ src/edu/pitt/cs1635group3/SplashScreenActivity.java
@@ -0,0 +1,49 @@
+package edu.pitt.cs1635group3;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+public class SplashScreenActivity extends Activity {
+    protected boolean _active = true;
+    protected int _splashTime = 5000;
+    
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        
+        // thread for displaying the SplashScreen
+        Thread splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    while(_active && (waited < _splashTime)) {
+                        sleep(100);
+                        if(_active) {
+                            waited += 100;
+                        }
+                    }
+                } catch(InterruptedException e) {
+                    // do nothing
+                } finally {
+                    finish();
+                    startActivity(new Intent("edu.pitt.cs1635group3.SociaListActivity"));
+                    stop();
+                }
+            }
+        };
+        splashTread.start();
+    }
+    
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            _active = false;
+        }
+        return true;
+    }
+}
