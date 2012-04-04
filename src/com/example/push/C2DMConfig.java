17126e8505cedb00d3c6c4ab1e17f9764f1d45cf
Kim Cooperrider <kim.cooperrider@gmail.com>
2012-04-04 02:05:48 -0400

    C2DM works

---
+++ src/com/example/push/C2DMConfig.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.push;
+
+/**
+ * Configuration settings for the Android client.
+ */
+public class C2DMConfig {
+
+	/**
+	 * Enabling this is will turn on sync UI icons that notify the user of
+	 * pending and active sync status. This is useful for debugging, but
+	 * generally not recommended for real applications, as sync on Android is
+	 * intended to be unobtrusive. Users can get the same sync status info by
+	 * going to Settings > Accounts & Sync.
+	 */
+	public static final boolean ENABLE_SYNC_UI = true;
+	public static final String C2DM_GOOGLE_ACCOUNT = "zebrafish.technologies@gmail.com";
+
+	@SuppressWarnings("unchecked")
+	public static String makeLogTag(Class cls) {
+		String tag = "PushDemo_" + cls.getSimpleName();
+		return (tag.length() > 23) ? tag.substring(0, 23) : tag;
+	}
+
+}
--- src/edu/pitt/cs1635group3/Activities/HomeActivity.java
+++ src/edu/pitt/cs1635group3/Activities/HomeActivity.java
@@ -1,27 +1,129 @@
 package edu.pitt.cs1635group3.Activities;
 
+import com.example.push.*;
+import com.google.android.c2dm.*;
+
 import edu.pitt.cs1635group3.R;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class HomeActivity extends Activity {
 	/** Called when the activity is first created. */
+	private static Activity curActivity = null;
+	private EditText messageText = null;
+	
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("GOT", "TO THIS POINT 1");
-        
+        Log.d("HOME ACTIVITY", "Got here");
         setContentView(R.layout.dashboard);
-        Log.d("GOT", "TO THIS POINT 2");
+		curActivity = this;
+        
+        
+        new AlertDialog.Builder(this)
+		.setTitle("Push Notifications")
+		.setMessage(getString(R.string.push_explain))
+		.setNegativeButton("No thanks",new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog,
+					int which) {
+				// do nothing
+			}
+		})
+		.setPositiveButton("Sure!",
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog,
+							int which) {
+						showPushNotificationDialog();
+					}
+				}).show();
+        
+        
         
     }
-	public void myLists(View v) {
+	
+    public void myLists(View v) {
 
 		Intent intent = new Intent(HomeActivity.this, SociaListActivity.class);
 		startActivityForResult(intent, 0);
 
+	}
+    
+    /**
+	 * Show push notification dialog.
+	 */
+	public void showPushNotificationDialog() {
+		Log.i("CLICKED BTN", "Show push notification dialog");
+
+		final SharedPreferences prefs = getSharedPreferences(
+				C2DMessaging.PREFERENCE, Context.MODE_PRIVATE);
+		if (C2DMessaging.shouldRegisterForPush(getApplicationContext())) {
+			if ((Build.VERSION.SDK_INT >= 8)
+					&& (C2DMessaging
+							.shouldRegisterForPush(getApplicationContext()))) {
+				C2DMReceiver.refreshAppC2DMRegistrationState(
+						getApplicationContext(), true);
+			}
+		}
+		
+		if (prefs.contains("dm_registration")) {
+			return;
+		}
+		
+
+		Log.i("PUSH", "Here 1");
+
+		if (Build.VERSION.SDK_INT >= 8) {
+
+			Editor e = prefs.edit();
+			e.putString("dm_registration", "");
+			e.commit();
+			C2DMessaging.setRegisterForPush(getApplicationContext(), true);
+			C2DMReceiver.refreshAppC2DMRegistrationState(
+					getApplicationContext(), true);
+
+		} else {
+			final Dialog dialogok = new Dialog(this);
+
+			dialogok.setContentView(R.layout.dialog_ok);
+			dialogok.setOwnerActivity(this);
+			dialogok.setTitle(getString(R.string.app_name));
+			TextView tv = (TextView) dialogok
+					.findViewById(R.id.textViewDialogMessage);
+			tv.setText("This Phone does not support push notification");
+			Button ok_btn = (Button) dialogok.findViewById(R.id.buttonOK);
+			OnClickListener l = new OnClickListener() {
+				public void onClick(View v) {
+					dialogok.dismiss();
+					Editor e = prefs.edit();
+					e.putString("dm_registration", "");
+					e.commit();
+					C2DMessaging.setRegisterForPush(getApplicationContext(),
+							false);
+				}
+			};
+			ok_btn.setOnClickListener(l);
+			dialogok.show();
+		}
+	} // end showPushNotificationDialog()
+	
+
+	public static Activity getCurrentActivity() {
+		// TODO Auto-generated method stub
+		return curActivity;
 	}
 }
